054134139986f2e8082496b43aa0600cef7ce1d4
Daniel Mateu <danielmateu86@gmail.com>
2023-01-29 14:31:27 +0100

    Rutas terminadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { PorCapitalComponent } from "./app/pais/pages/por-capital/por-capital.component";
-import { PorPaisComponent } from './app/pais/pages/por-pais/por-pais.component';
-import { PorRegionComponent } from "./app/pais/pages/por-region/por-region.component";
-import { VerPaisComponent } from "./app/pais/pages/ver-pais/ver-pais.component";
+import { PorCapitalComponent } from "./pais/pages/por-capital/por-capital.component";
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from "./pais/pages/por-region/por-region.component";
+import { VerPaisComponent } from "./pais/pages/ver-pais/ver-pais.component";
 
 
 const routes: Routes = [
