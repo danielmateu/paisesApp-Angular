4fe2a2fc6883186ba30dcaf89acde1cfcb329bae
Daniel Mateu <danielmateu86@gmail.com>
2023-01-29 21:33:08 +0100

    Llenando la tabla de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class PorPaisComponent {
 
   termino: string = ''
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService){}
 
@@ -21,11 +23,13 @@ export class PorPaisComponent {
       .subscribe({
         next: (paises) => {
           console.log(paises)
+          this.paises = paises
 
         },
         error: (err) => {
           console.log(err)
           this.hayError = true
+          this.paises = []
         }
       })
   }
