e17ec636efb2463a1d80e99f48bf4dedd57ce57b
Daniel Mateu <danielmateu86@gmail.com>
2023-01-30 09:57:07 +0100

    debounceTime en el input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,16 +1,41 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import {Subject} from 'rxjs'
+import {debounceTime} from 'rxjs/operators'
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
-  styleUrls: ['./pais-input.component.css']
+  styleUrls: []
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
 
-  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+
+  @Output() onEnter   : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
+
   termino: string = '';
+
+  ngOnInit(): void {
+  this.debouncer
+  .pipe(debounceTime(300))
+  .subscribe(valor => {
+    // console.log('debouncer:', valor)
+    this.onDebounce.emit(valor)
+  })
+  }
 
   buscar() {
     this.onEnter.emit(this.termino)
+
   }
+
+  teclaPresionada(event: any){
+    const valor = event.target.value;
+    // console.log(valor)
+    // console.log(this.termino)
+    this.debouncer.next(this.termino);
+  }
+
+
 }
